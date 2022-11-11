eabc2babe18fab7fc47021602e3a3d798d38a85b
ron4ldblue <ronaldblue54@gmail.com>
2022-11-10 22:17:58 -0300

    terceiro commit

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,5 +1,25 @@
 public class Operadores {
     public static void main(String[] args) {
-    
-    }
+        String concatenacao ="?";
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);
+    }   
 }
